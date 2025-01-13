debb8ac3b3b83f028d952bc30794854659547bde
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-13 15:02:19 +0530

    customize the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav>
-        navbar
+    <nav className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+            <Link href="/" className="text-xl font-bold">PiTalk</Link>
+            <ul className="flex space-x-4">
+                <li>
+                    <Link href="/" className="hover:text-gray-200">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about" className="hover:text-gray-200">About</Link>
+                </li>
+                <li>
+                    <Link href="/contact" className="hover:text-gray-200">Cotact</Link>
+                </li>
+            </ul>
+        </div>
     </nav>
   )
 }
