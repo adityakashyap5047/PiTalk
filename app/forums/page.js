2e23aa352b42f165a495f3ad66949796db8510ba
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-17 23:16:15 +0530

    defining the route on clicking on forums button

--- app/forums/page.js
+++ app/forums/page.js
@@ -1,31 +1,38 @@
 import React from 'react'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const topics = [
     {   
         text: "Python",
         img: "/python.webp",
-        desc: "Lets discuss everything related to Python"
+        desc: "Lets discuss everything related to Python",
+        slug: "python"
     },
     {
         text: "JavaScript",
         img: "/js.webp",
-        desc: "Learn and share JavaScript knowledge"
+        desc: "Learn and share JavaScript knowledge",
+        slug: "js"
     },
     {
         text: "React",
         img: "/reactjs.webp",
-        desc: "Build amazing user interface with React"
+        desc: "Build amazing user interface with React",
+        slug: "react"
     },
     {
         text: "CSS",
         img: "/css.webp",
-        desc: "Explore the world of designing and CSS"
+        desc: "Explore the world of designing and CSS",
+        slug: "css"
     },
     {
         text: "Web Development",
         img: "/html.webp",
-        desc: "Discuss front-end, back-end, and full-stack web development."
+        desc: "Discuss front-end, back-end, and full-stack web development.",
+        slug: "web"
     }
 ];
 
@@ -40,7 +47,9 @@ const Forums = () => {
                     <Image alt="a" src={topic.img} width={74} height={74}/>
                     <h2 className="text-2xl">{topic.text}</h2>
                     <p className="px-3">{topic.desc}</p>
-                    <button className='px-4 border border-slate-300 py-2'>Discuss now</button>
+                    <Link href={`/chat/${topic.slug}`}>
+                        <Button className='px-4 border border-slate-300 py-2'>Discuss now</Button>
+                    </Link>
                 </div>
             })}
         </div>
