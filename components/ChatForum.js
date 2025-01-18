3dd758aaf93d60c77937269d7ea76f459de90db2
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-19 00:35:02 +0530

    completing the manual user creation

--- components/ChatForum.js
+++ components/ChatForum.js
@@ -2,21 +2,26 @@
 
 import { useState, useEffect } from 'react';
 import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { useUser } from '@clerk/nextjs';
 
 import 'stream-chat-react/dist/css/v2/index.css';
 
 const apiKey = process.env.STREAM_API_KEY;
-const userId = 'jolly-firefly-1';
-const userName = 'jolly';
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9sbHktZmlyZWZseS0xIiwiZXhwIjoxNzM3MTU0NzkyfQ.7Q4faTSsHAouN57plymo77tcYoCvr7GWqVNIgRjBhvA';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8ycm9Sekh4U3lCZEM4OVVKOHFWQlBhTDNIdFMifQ.UYrS-7Xt3r6eVvQUEeAMh-5KpBkXIvD_PZlmJT-eVxo';
 
-const user = {
-  id: userId,
-  name: userName,
-  image: `https://getstream.io/random_png/?name=${userName}`,
-};
 
-const ChatForum = () => {
+const ChatForum = ({slug}) => {
+
+  const clerkUser = useUser();
+
+  const userId = clerkUser.user?.id;
+  const userName = clerkUser.user?.firstName;
+
+  const user = {
+    id: userId,
+    name: userName,
+    image: `https://getstream.io/random_png/?name=${userName}`,
+  };
   const [channel, setChannel] = useState();
   const client = useCreateChatClient({
     apiKey,
@@ -27,14 +32,14 @@ const ChatForum = () => {
   useEffect(() => {
     if (!client) return;
 
-    const channel = client.channel('messaging', 'custom_channel_id', {
+    const channel = client.channel('messaging', slug, {
       image: 'https://getstream.io/random_png/?name=react',
-      name: 'Talk about React',
+      name: "Discussion",
       members: [userId],
     });
 
     setChannel(channel);
-  }, [client]);
+  }, [client, slug]);
 
   if (!client) return <div>Setting up client & connection...</div>;
 
