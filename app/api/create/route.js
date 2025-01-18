a360b311f5aef96683191550a98acbbca9cf16bc
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-18 23:55:07 +0530

    creating the token in stream with the same user_id as clerk

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -2,11 +2,12 @@ import { StreamChat } from "stream-chat";
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
-const user_id = "user_id";
+const user_id = "user_2roRzHxSyBdC89UJ8qVBPaL3HtS";     //same as the clerk user_id
 
 export async function GET() {
     const ServerClient = StreamChat.getInstance(api_key, api_secret);
     //Create User Token
     const token = ServerClient.createToken(user_id);
-    return Response.json({ message: "Welcom" })
+    console.log(token);
+    return Response.json({ message: "Welcome" })
 }
