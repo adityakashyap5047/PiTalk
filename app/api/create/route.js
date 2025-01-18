6d80553d8a6b49b07d085ea6614d9f4a490e85bc
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-18 23:50:30 +0530

    creating user token in stream

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,3 +1,12 @@
+import { StreamChat } from "stream-chat";
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const user_id = "user_id";
+
 export async function GET() {
-    return Response.json({message: "Welcom"})
+    const ServerClient = StreamChat.getInstance(api_key, api_secret);
+    //Create User Token
+    const token = ServerClient.createToken(user_id);
+    return Response.json({ message: "Welcom" })
 }
