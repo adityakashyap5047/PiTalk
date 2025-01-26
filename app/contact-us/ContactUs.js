21ccddcad1041a1bb9ce354b72923a2a58623a8a
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-26 22:49:11 +0530

    complete all the functionallity for passcode generate

--- app/contact-us/ContactUs.js
+++ app/contact-us/ContactUs.js
@@ -83,7 +83,6 @@ const SignupFormDemo = () => {
       setIsSentPasscode(true);
     } catch (error) {
       setIsSendingPasscode(false);
-
       console.log(error)
     }
 
@@ -113,10 +112,7 @@ const SignupFormDemo = () => {
   }
 
   const handleSubmit = async (e) => {
-    if(!isVerified){
-      console.log("not verified");
-      return;
-    }
+    e.preventDefault();
     console.log("form submitted");
   }
 
@@ -318,7 +314,7 @@ const SignupFormDemo = () => {
             />      
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit">
+          type="submit" disabled={!isSentPasscode} onClick={handleSubmit}>
           Submit &rarr;
           <BottomGradient />
         </button>
