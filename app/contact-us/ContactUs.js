b88d4636f4fc0b6e8e278aa4af4d6822d321e123
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-26 12:07:27 +0530

    completing the form validation

--- app/contact-us/ContactUs.js
+++ app/contact-us/ContactUs.js
@@ -12,7 +12,7 @@ import { svg_retry, svg_verified, svg_verify, svg_verifying } from "@/components
 import { Select } from "@/components/ui/select";
 import { useState } from 'react';
 import { ToastAction } from "@/components/ui/toast"
-import { Form } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { useToast } from '@/hooks/use-toast';
 
 const SignupFormDemo = () => {
@@ -23,15 +23,26 @@ const SignupFormDemo = () => {
 
   //schema for form validation
   const schema = z.object({
-    firstName: z.string({message: "First Name can not be empty"}),
+    firstName: z.string().nonempty("First Name cannot be empty"),
+    lastName: z.string().nonempty("Last Name cannot be empty"),
+    email: z.string().email({message: "Invalid email address"}),
+    verifyEmail: z.string().nonempty("Give your verification code"),
+    category: z.string().nonempty("Select a Category"),
+    title: z.string().nonempty("Please enter a title"),
+    description: z.string().nonempty("give the description about your query")
   })
 
   //zod implementation
-  const form = useForm<z.infer>({
+  const form = useForm({
     resolver: zodResolver(schema),
     defaultValues: {
+      firstName: '',
+      lastName: '',
       email: '',
-      password: ''
+      verifyEmail: '',
+      category: '',
+      title: '',
+      description: ''
     }
   })
 
@@ -45,7 +56,7 @@ const SignupFormDemo = () => {
   //   })
   // }
 
-  const handleSubmit = async (data) => {
+  const handleSubmit = async (e) => {
     console.log("form submitted");
   }
 
@@ -61,31 +72,87 @@ const SignupFormDemo = () => {
         baseHue={10}
       > */}
       <Form {...form}>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
         <div
           className="flex flex-col md:flex-row md:space-x-2">
-          <LabelInputContainer>
-            <Label htmlFor="firstname" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">First name</Label>
-            <Input id="firstname" placeholder="Enter your First Name" type="text"/>
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Last name</Label>
-            <Input id="lastname" placeholder="Enter your Last Name" type="text"/>
-          </LabelInputContainer>
+            <FormField
+              name="firstName"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-1/2">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">FirstName</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your First Name"
+                      {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="lastName"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-1/2">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">FirstName</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your Last Name"
+                      {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
         </div>
         <div className="flex flex-col md:flex-row md:space-x-2">
-  <LabelInputContainer className="mb-4 md:w-1/2">
-    <Label htmlFor="email" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">
-      Email Address
-    </Label>
-    <Input id="email" placeholder="Enter your email id here" type="email"/>
-  </LabelInputContainer>
-  <LabelInputContainer className="mb-4 md:w-1/4">
-    <Label htmlFor="password" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">
-      Email Verification Code
-    </Label>
-    <Input id="password" placeholder="Enter your password here" type="password" />
-  </LabelInputContainer>
+        <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-1/2">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">FirstName</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your Email Address"
+                      {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        <FormField
+              name="verifyEmail"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-1/4">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Veirfy your Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter verification code"
+                      {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
   <Button variant="secondary" className="mt-10 h-9" type="button" onClick={() => {
         toast({
           title: "Scheduled: Catch up ",
@@ -97,7 +164,8 @@ const SignupFormDemo = () => {
       }}
 >Verify your Email {svg_verify}</Button>
 </div>
-        {/* <div className="flex flex-col md:flex-row md:space-x-2">
+        <div>
+          {/* <div className="flex flex-col md:flex-row md:space-x-2">
   <LabelInputContainer className="mb-4 md:w-1/2">
     <Label htmlFor="email" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">
       Contact Number
@@ -124,26 +192,69 @@ const SignupFormDemo = () => {
       {svg_retry}
     </Button> 
 </div> */}
-<div className="flex flex-col md:flex-row md:space-x-2">
-        <LabelInputContainer className="mb-4 md:w-1/2">
-          <Label htmlFor="queryType" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Select the query</Label>
-          <Select id="queryType">
-          <option value="">Select a query</option>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="title" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Title</Label>
-          <Input id="title" placeholder="Enter your title"/>
-        </LabelInputContainer>
         </div>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="description" className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Description</Label>
-          <Textarea id="description" placeholder="Write a description"/>
-        </LabelInputContainer>
-      
+<div className="flex flex-col md:flex-row md:space-x-2">
+<FormField
+              name="category"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-1/4">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Select the Category</FormLabel>
+                  <FormControl>
+                              <Select {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } >
+                                  <option value="">Select a Category</option>
+                                  <option value="option1">Option 1</option>
+                                  <option value="option2">Option 2</option>
+                                  <option value="option3">Option 3</option>
+                              </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="title"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-3/4">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter the Title"
+                      {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        </div>
+        <FormField
+              name="description"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="md:w-full">
+                  <FormLabel className="text-neutral-300 text-sm max-w-sm mt-2 dark:text-neutral-600">Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Write a description"
+                      {...field}
+                      onChange={(e) => {
+                          field.onChange(e)
+                        }
+                      } 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />      
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit">
