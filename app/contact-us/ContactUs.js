a618525efd4357c369ea9700b5bcea06f146017b
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-28 14:04:11 +0530

    integrate the animation in button

--- app/contact-us/ContactUs.js
+++ app/contact-us/ContactUs.js
@@ -437,6 +437,7 @@ const SignupFormDemo = () => {
                     <Input
                       placeholder="Enter the Title"
                       {...field}
+                      disabled={isSubmitting}
                       onChange={(e) => {
                         field.onChange(e);
                       }}
@@ -469,15 +470,14 @@ const SignupFormDemo = () => {
               </FormItem>
             )}
           />
-          <button
+          <Button
             className="mt-4 md:mt-9 w-1/2 mx-auto bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
             disabled={!isSentPasscode || !isVerifiedPasscode || isSubmitting }
             onClick={handleSubmit}
           >
-            Submit &rarr;
-            <BottomGradient />
-          </button>
+            {!isSubmitting ? <span className="animate-bounce"> Submitting {svg_verifying} </span> : <span>Submit &rarr; <BottomGradient /></span> }
+          </Button>
           {isSubmitError && (
             <p className="text-sm text-red-600">
               Please fill all the above field.
